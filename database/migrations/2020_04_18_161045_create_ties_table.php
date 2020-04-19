a3db22ae80638d08e8a3f97dd1af4f446daf6038
Marie Anne Chartier <marie-anne@soundcharts.com>
2020-04-19 14:43:47 +0200

    fix tables

--- database/migrations/2020_04_18_161045_create_ties_table.php
+++ database/migrations/2020_04_18_161045_create_ties_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Tie;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,17 +16,17 @@ class CreateTiesTable extends Migration
     {
         Schema::create('ties', function (Blueprint $table) {
 
-            $table->foreignId('src');
-            $table->foreignId('dest');
-            $table->foreignId('ref')->nullable();
+            $table->foreignId(Tie::SRC_COLUMN);
+            $table->foreignId(Tie::DEST_COLUMN);
+            $table->foreignId(Tie::REF_COLUMN)->nullable();
             $table->integer('rank')->nullable();
             $table->timestamps();
 
-            $table->primary(['src', 'dest', 'ref']);
+            $table->primary([Tie::REF_COLUMN, Tie::SRC_COLUMN, Tie::DEST_COLUMN]);
 
-            $table->foreign('src')->references('id')->on('nodes');
-            $table->foreign('dest')->references('id')->on('nodes');
-            $table->foreign('ref')->references('id')->on('nodes');
+            $table->foreign(Tie::SRC_COLUMN)->references('id')->on('nodes');
+            $table->foreign(Tie::DEST_COLUMN)->references('id')->on('nodes');
+            $table->foreign(Tie::REF_COLUMN)->references('id')->on('nodes');
 
         });
     }
